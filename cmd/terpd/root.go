a2e8d2c20b41e56664045f1d22a72e345c8d6622
discoverdefiteam <discover.defi@gmail.com>
2023-04-05 04:06:01 +0000

    rename to Terp Daemon

--- cmd/terpd/root.go
+++ cmd/terpd/root.go
@@ -61,7 +61,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   version.AppName,
-		Short: "Wasm Daemon (server)",
+		Short: "Terp Network Daemon (server)",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
