0e5cf81df267a211b942d7905764cd57388d3b3e
hard-nett <hardnettt@proton.me>
2023-06-15 23:12:32 -0700

    static check for math/rand disabled

--- cmd/terpd/root.go
+++ cmd/terpd/root.go
@@ -131,8 +131,8 @@ func initAppConfig() (string, interface{}) {
 	//   own app.toml to override, or use this default value.
 	//
 	// In simapp, we set the min gas prices to 0.
-	srvCfg.MinGasPrices = "0stake"
-	// srvCfg.BaseConfig.IAVLDisableFastNode = true // disable fastnode by default
+	srvCfg.MinGasPrices = "0.0002uthiol"
+	srvCfg.BaseConfig.IAVLDisableFastNode = false // disable fastnode by default
 
 	customAppConfig := CustomAppConfig{
 		Config: *srvCfg,
